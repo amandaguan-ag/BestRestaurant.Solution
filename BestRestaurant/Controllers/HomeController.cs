2f656f7dbeaf24c2d77ea42e882498027e38cb8f
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-17 17:09:00 -0700

    add resturantController

--- BestRestaurant/Controllers/HomeController.cs
+++ BestRestaurant/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using BestRestaurant.Models;
-using System.Collections.Generic;
 
 namespace BestRestaurant.Controllers
 {
@@ -10,22 +8,7 @@ namespace BestRestaurant.Controllers
         [HttpGet("/")]
         public ActionResult Index()
         {
-
-            List<Restaurant> allRestaurants = Restaurant.GetAll();
-            return View(allRestaurants);
-        }
-
-        [HttpGet("/restaurants/new")]
-        public ActionResult CreateForm()
-        {
             return View();
-        }
-
-        [HttpPost("/restaurants")]
-        public ActionResult Create(string description)
-        {
-            Restaurant myRestaurant = new Restaurant(description);
-            return RedirectToAction("Index");
         }
 
     }
